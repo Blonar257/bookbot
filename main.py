07b52a23d52d5f527cf3fa140fc991117adaaf65
Stefan Schrage <stefan.schrage@googlemail.com>
2025-09-07 20:45:46 +0200

    read book and word counting

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("greetings boots")
+def get_book_text (filepath: str) -> str:
+    with open (filepath) as f:
+        file_content = f.read()
+    return file_content
+
+def word_count(text: str) -> None:
+    splitted: list[str] = text.split()
+    num_words = len(splitted)
+    print(f"{num_words} words found in the document")
+
+def main():
+    text = get_book_text("./books/frankenstein.txt")
+    word_count(text)
+
+main()
