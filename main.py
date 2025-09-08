53d6062c3f26917ed692d1e3e974e749303eda55
Stefan Schrage <stefan.schrage@googlemail.com>
2025-09-08 19:24:05 +0200

    Path as args and new books

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import get_num_words, get_num_character, sorted_list
 
 def get_book_text (filepath: str) -> str:
@@ -6,7 +7,12 @@ def get_book_text (filepath: str) -> str:
     return file_content
 
 def main():
-    filepath = r"./books/frankenstein.txt"
+    # filepath = r"./books/frankenstein.txt"
+    if len(sys.argv) <= 1:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        filepath = sys.argv[1]
     text = get_book_text(filepath)
     num_words = get_num_words(text)
     num_chars_dict = get_num_character(text)
